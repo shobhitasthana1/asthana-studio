e1ab7fb26662843a5a8d387f7957fefabb49c30f
Shobhit Asthana <114984222+shobhitasthana1@users.noreply.github.com>
2026-07-16 16:36:52 +0530

    Implement Gooey Morph image animation on page 3

--- script.js
+++ script.js
@@ -150,60 +150,10 @@ page1Animation();
 function page3Animation() {
     Shery.imageEffect(".image-box", {
         style: 6,
-        debug: true,
-
-        noiseDetail: 7.44,
-        distortionAmount: 2.98,
-        scale: 36.36,
-        speed: 0.79,
-
-        zindex: -9996999,
-        aspect: 0.7272614591338407,
-        ignoreShapeAspect: true,
-
-        shapePosition: {
-            x: 0,
-            y: 0
-        },
-
-        shapeScale: {
-            x: 0.5,
-            y: 0.5
-        },
-
-        shapeEdgeSoftness: 0,
-        shapeRadius: 0,
-
-        scrollLerp: 0.07,
-
+        // debug: true,
+        config: {"noiseDetail":{"value":7.44,"range":[0,100]},"distortionAmount":{"value":2.98,"range":[0,10]},"scale":{"value":36.36,"range":[0,100]},"speed":{"value":0.79,"range":[0,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7272614591338407},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0.5,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.27,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":1.76,"range":[0,10]},"metaball":{"value":0.44,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
         gooey: true,
-        infiniteGooey: true,
-
-        growSize: 4,
-
-        durationOut: 1,
-        durationIn: 1.5,
-
-        displaceAmount: 0.5,
-
-        masker: false,
-        maskVal: 1,
-
-        scrollType: 0,
-        geoVertex: 1,
-
-        noEffectGooey: true,
-
-        onMouse: 0,
-
-        noise_speed: 1.15,
-        metaball: 0.61,
-
-        discard_threshold: 0.47,
-        antialias_threshold: 0.02,
-
-        noise_height: 0.5,
-        noise_scale: 21.37
+       
     });
 }
 
