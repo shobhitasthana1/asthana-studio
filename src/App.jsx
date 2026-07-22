ca6601002cbf44e338c9ea0bd8095ef629cc9ece
Shobhit Asthana <114984222+shobhitasthana1@users.noreply.github.com>
2026-07-22 17:21:52 +0530

    feat: add Work component to hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Work from './components/Work'
 
 const App = () => {
   return (
     <div className='w-full min-h-screen text-white bg-[#161418]'>
       <Navbar />
+      <Work />
     </div>
   )
 }
---
+++ src/components/Work.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Work = () => {
+
+var image = [
+  {url: "", top: 0, left: 0, isActive: false}
+]
+
+  return (
+    <div className='w-full'>
+
+     <div className='max-w-7xl mx-auto flex items-center justify-center'>
+        <h1 className='text font-impasse text-[22vw] font-black select-none'>WORK</h1>
+     </div>
+
+    </div>
+  )
+}
+
+export default Work
